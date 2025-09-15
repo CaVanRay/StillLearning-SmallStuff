d00108393e8a407d9a812ab35d6251b0f5e70d62
Cavan Theiss <cavanrt@gmail.com>
2025-09-15 09:26:23 -0500

    Update squares.cpp

--- squares.cpp
+++ squares.cpp
@@ -6,6 +6,18 @@ using namespace std;
 
 main(){
 
+  int initial = 1;
+  string toConvert;
+  vector<int> TopV, BotV, ProdV;
+
+  // String/Digit Converter
+  toConvert = to_string(initial);
+  for (char c : toConvert){
+      TopV.insert(TopV.begin(),(c - '0'));
+    }
+  int tempMult, carry = 0;
+
+  for (int y
 
   
 }
