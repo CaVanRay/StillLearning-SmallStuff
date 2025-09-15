0a664471e1f1400cbf4315a91fff07702f876143
Cavan Theiss <cavanrt@gmail.com>
2025-09-15 09:31:22 -0500

    Update squares.cpp

--- squares.cpp
+++ squares.cpp
@@ -12,17 +12,71 @@ main(){
 
   for(int initial = 1; initial < 101; initial++){
     for(int secondary = 1; secondary < 101; secondary++){
+
+        // String/Digit Converter
+    toConvert = to_string(inital);
+    for (char c : toConvert) { 
+        TopV.insert(TopV.begin(),(c - '0'));
+    }
+    
+    int tempMult, carry = 0;
+
+    for (int y = (factInput - 1); y >= 1; y--){
+
+        BotV.clear();
+        toConvert = to_string(y);
+        for (char c : toConvert) {
+            BotV.insert(BotV.begin(), (c - '0'));
+        }
+
+        ProdV.clear();
+        ProdV.resize(TopV.size() + BotV.size() + 1);
+        
+        for (size_t i = 0; i < TopV.size(); i++) {
+            int proLocation = i;
+            
+            for (size_t j = 0; j < BotV.size(); j++) {               
+                tempMult = (TopV[i] * BotV[j]);
+                
+                while(tempMult > 9){
+                    
+                    tempMult = tempMult - 10;
+                    carry = carry + 1;
+                }
+                
+            ProdV[proLocation] += tempMult;
+                while(ProdV[proLocation] > 9){
+                    ProdV[proLocation] -= 10;
+                    carry++;
+                }
+            ProdV[proLocation + 1] += carry;
+                while(ProdV[proLocation + 1] > 9){
+                    ProdV[proLocation + 1] -= 10;
+                    ProdV[proLocation + 2] += 1;
+                }                
+            carry = 0;
+                
+            proLocation++;
+            }
+        }
+        // I'm removing the leading zeros from the product
+        int newSize = ProdV.size();
+        while (newSize > 1 && ProdV[newSize - 1] == 0) {
+            newSize--;
+        }
+        ProdV.resize(newSize);
+        
+    TopV = ProdV; 
+    ProdV.clear();
+
+    }
+    cout << "Product = ";
+    for (int k = (TopV.size() - 1); k >= 0; k--){
+        cout << TopV[k];
+    } 
+
       
     }
   }
-  // String/Digit Converter
-  toConvert = to_string(initial);
-  for (char c : toConvert){
-      TopV.insert(TopV.begin(),(c - '0'));
-    }
-  int tempMult, carry = 0;
-
-  for (int y
-
-  
+  return 0;
 }
